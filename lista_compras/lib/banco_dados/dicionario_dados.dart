75e242ba42f6ac8424f1b3749e4d17528ab4abc6
Ryan Cordeiro <62850392+ryanccor@users.noreply.github.com>
2022-05-17 21:12:46 -0300

    Add files via upload

---
+++ lista_compras/lib/banco_dados/dicionario_dados.dart
@@ -0,0 +1,15 @@
+class DicionarioDados{
+  static const arquivoBancoDados = 'lista_compras.bd';
+  static const tabelaTipoProduto = 'tipo_produto';
+  static const tabelaProduto = 'produto';
+  static const tabelaListaCompra = 'lista_compra';
+  static const tabelaItemListaCompra = 'item_lista_compra';
+  static const idTipoProduto = 'idTipoProduto';
+  static const idProduto = 'idProduto';
+  static const idListaCompra = 'idListaCompra';
+  static const nome = 'nome';
+  static const quantidade = 'quantidade';
+  static const numeroItem = 'numero_item';
+  static const selecionado = 'selecionado';
+  static const unidade = 'unidade';
+}
